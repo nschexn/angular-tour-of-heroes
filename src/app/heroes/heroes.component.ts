c36366af3317445aa764423ed678400d0b626340
Neal Schexnider <nschexn@hotmail.com>
2020-07-14 11:53:35 -0500

    lesson 5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,12 +11,8 @@ import { MessageService } from '../message.service'
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(
-    private heroService: HeroService,
-    private messagesService: MessageService
-    ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,11 +20,6 @@ export class HeroesComponent implements OnInit {
   
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messagesService.add(`HeroesComponent: Selected Hero id=${hero.id}, name=${hero.name}`)
   }
 
 }
